ee3be3caf4267f9a5d4c5cee1f828f0c143240a2
JulianG <juliangaramendy@gmail.com>
2019-08-20 08:25:33 +0100

    react component using hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useGamesAPI } from "./model/useGamesAPI";
+import { makeGameAPI } from "./api/games.api";
+import { jsonFetch } from "./api/json.fetch";
+import { GameStatus } from './model/model';
+
+const gameApi = makeGameAPI(jsonFetch, "http://localhost:3001/");
 
 const App: React.FC = () => {
+  const { games, setGameStatus } = useGamesAPI(gameApi);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Some Commodore 64 Games</h1>
+      {games.map(game => (
+        <div key={game.id}>
+          <h3>
+            {game.title} ({game.year})
+          </h3>
+          <p>
+            Genre: {game.genre}
+            <br />
+            Wikipedia: <a href={game.url}>{game.url}</a>
+            <br />
+            <Status
+              status={game.status}
+              markAsFinished={() => setGameStatus(game.id, "finished")}
+            />
+          </p>
+        </div>
+      ))}
+    </>
   );
+};
+
+function getStatusText(status: GameStatus): string {
+  return {
+    "not-started": "Not started",
+    started: "Started",
+    finished: "Finished"
+  }[status];
 }
+
+const Status: React.FC<{ status: GameStatus; markAsFinished: Function }> = ({
+  status,
+  markAsFinished
+}) => {
+  if (status !== "finished") {
+    return (
+      <p>
+        Status: {getStatusText(status)} &nbsp;{" "}
+        <button onClick={() => markAsFinished()}>Mark as Finished</button>
+      </p>
+    );
+  } else {
+    return <p>Status: {getStatusText(status)}</p>;
+  }
+};
 
 export default App;
